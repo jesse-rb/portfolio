816a15608ea9d94b8ab5c2d0d8ee8fa054f121d0
jesse-rb <jesse.reynekebarnard@gmail.com>
2021-05-26 17:20:44 +1200

    Add GitHub and LinkedIn links in portfolio header

--- React/src/Components/Layouts/Top.js
+++ React/src/Components/Layouts/Top.js
@@ -4,7 +4,11 @@ import './Top.css'
 export default function Top() {
     return (
         <div id="top">
-            <h1>Jesse Reyneke-Barnard</h1>
+            <div id="details">
+                <h1>Jesse Reyneke-Barnard</h1>
+                <a target="_blank" rel="noopener noreferrer"href="https://github.com/jesse-rb">GitHub</a>
+                <a target="_blank" rel="noopener noreferrer"href="https://www.linkedin.com/in/jesse-reyneke-barnard/">LinkedIn</a>
+            </div>
             <div id="navbar">
                 <div><a href="/">Home</a></div>
                 <div><a href="/projects">Projects</a></div>
