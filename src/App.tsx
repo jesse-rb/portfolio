e2fbca1fd2a4e656bb930665a978bd89e474b7ff
jesse-rb <jesse.reynekebarnard@gmail.com>
2026-05-06 00:15:36 +1200

    Init porject with a cozy fireplace theme

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import Tile from "./tile/Tile";
+import AssetEnrolmy from "./assets/enrolmy.png";
+import AssetBasis from "./assets/basis.png";
+import AssetEnrolmyLogo from "./assets/enrolmy.logo.png";
+import AssetBasisLogo from "./assets/basis.logo.png";
+
+function App() {
+    return (
+        <>
+            <div className="text-center">
+                <h1>Jesse Reyneke-Barnard</h1>
+            </div>
+            <h2>Past Work Experience</h2>
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+                <Tile
+                    headlines={["Backend Engineer"]}
+                    title="Basis"
+                    src={AssetBasis}
+                    logo={AssetBasisLogo}
+                />
+                <Tile
+                    headlines={[
+                        "Software Developer",
+                        "Intern Software Developer",
+                    ]}
+                    title="Enrolmy Software"
+                    src={AssetEnrolmy}
+                    logo={AssetEnrolmyLogo}
+                />
+            </div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/tile/Tile.tsx
@@ -0,0 +1,143 @@
+import { useEffect, useMemo, useRef, useState, type MouseEvent } from "react";
+
+function Tile(props: {
+    title: string;
+    headlines: Array<string>;
+    src: string;
+    logo: string;
+}) {
+    const [transformOrigin, setTransformOrigin] = useState(
+        calcTransformOrigin(0, 0),
+    );
+
+    const [point, setPoint] = useState({
+        x: Math.floor(Math.random() * 50) - 25,
+        y: Math.floor(Math.random() * 50) - 25,
+    }); // Point percentage relative to center
+
+    const edgePoint = useMemo(() => {
+        // Scale factor to reach the nearest edge
+        // Whichever axis is "more extreme" determines which edge we choose
+        const scale = 50 / Math.max(Math.abs(point.x), Math.abs(point.y));
+
+        const edgeX = point.x * -scale;
+        const edgeY = point.y * scale;
+
+        // Convert from "center-relative" (-50 to 50) to percentage (0 to 100)
+        return {
+            x: edgeX + 50,
+            y: edgeY + 50,
+        };
+    }, [point]);
+    const lastUpdate = useRef(0);
+
+    // Store edgePoint in state with a delayed update
+    const [smoothEdge, setSmoothEdge] = useState(edgePoint);
+
+    useEffect(() => {
+        const now = performance.now();
+        if (now - lastUpdate.current >= 175) {
+            lastUpdate.current = now;
+            setSmoothEdge(edgePoint);
+        }
+    }, [edgePoint]);
+
+    // Jitter loop
+    useEffect(() => {
+        const now = performance.now();
+        const lu = now - 175;
+        let raf: number;
+        const loop = () => {
+            if (now - lu >= 175) {
+                setPoint((prev) => ({
+                    x: prev.x + (Math.random() - 0.5) * 2 * 0.2,
+                    y: prev.y + (Math.random() - 0.5) * 2 * 0.2,
+                }));
+            }
+            raf = requestAnimationFrame(loop);
+        };
+        raf = requestAnimationFrame(loop);
+        return () => cancelAnimationFrame(raf);
+    });
+
+    function handleImgMouseMove(e: MouseEvent<HTMLImageElement>) {
+        const target = e.currentTarget;
+
+        const width = target.offsetWidth;
+        const left = target.offsetLeft;
+        const xRelativeToCentre = left + width / 2 - e.clientX;
+        const x = (xRelativeToCentre / (width / 2)) * 25;
+
+        const height = target.offsetHeight;
+        const top = target.offsetTop;
+        const yRelativeToCentre = e.clientY - (top + height / 2);
+        const y = (yRelativeToCentre / (height / 2)) * 25;
+
+        setPoint({ x, y });
+
+        setTransformOrigin(
+            calcTransformOrigin(xRelativeToCentre, yRelativeToCentre),
+        );
+    }
+
+    function calcTransformOrigin(x: number, y: number): string {
+        if (y === 0 && x !== 0) {
+            return x < 0 ? "origin-left" : "origin-right";
+        } else if (x === 0 && y !== 0) {
+            return y < 0 ? "origin-top" : "origin-bottom";
+        } else if (x > 0) {
+            return y < 0 ? "origin-top-right" : "origin-bottom-right";
+        } else if (x < 0) {
+            return y < 0 ? "origin-top-left" : "origin-bottom-left";
+        }
+        return "origin-center";
+    }
+
+    return (
+        <>
+            <div className="rounded-md max-w-96 gap-4 flex flex-col">
+                <div
+                    className="flex w-full h-fit rounded-md overflow-clip inset-shadow-sm"
+                    style={{
+                        background: `radial-gradient(circle at ${smoothEdge.x}% ${smoothEdge.y}%, #F2C4A0 -50%, transparent 100%)`,
+                    }}
+                    onMouseMove={handleImgMouseMove}
+                >
+                    <div className="h-10/12 m-auto">
+                        <img
+                            className={`${transformOrigin} h-full rounded-md duration-150`}
+                            src={props.src}
+                            style={{
+                                transform: `rotateX(${point.y}deg) rotateY(${point.x}deg)`,
+                                filter: `drop-shadow(${point.x}px ${-1 * point.y}px 10px #282828)`,
+                            }}
+                        ></img>
+                    </div>
+                </div>
+                <div className="px-4 pb-4 flex flex-col gap-2 h-full">
+                    {props.headlines.length > 1 ? (
+                        <ul className="flex flex-col gap-2">
+                            {props.headlines.map((headline) => {
+                                return (
+                                    <li className="leading-none border-l border-l-gray-400 pl-2">
+                                        {headline}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    ) : (
+                        <p className="leading-none border-l border-l-gray-400 pl-2">
+                            {props.headlines[0]}
+                        </p>
+                    )}
+                    <p className="mt-auto flex gap-2 items-center">
+                        <img className="h-5 rounded-sm" src={props.logo}></img>
+                        {props.title}
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Tile;
